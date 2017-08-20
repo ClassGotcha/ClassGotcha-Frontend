ad4763291780fba09a6ab5b25b9c51a458e32199
C0deZ Lee <c0dezlee@gmail.com>
2017-08-20 01:07:27 -0400

    Style Update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import store from './store'
 import App from 'views/components/App'
 import * as cookie from './utils/cookie'
 
-Vue.use(VueHead, { complement: 'ClassGotcha' })
+Vue.use(VueHead, {complement: 'ClassGotcha'})
 Vue.use(Router)
 Vue.use(Resource)
 Vue.use(Mask)
@@ -21,35 +21,35 @@ sync(store, router)
 // set csrf token for django
 Vue.http.headers.common['X-CSRFToken'] = cookie.getCookie('csrftoken')
 export const app = new Vue({
-    el: '#app',
-    data: {},
-    beforeCreate() {
-        const formData = { token: cookie.getCookie('token') }
-        this.$store.dispatch('tokenVerify', formData)
-        /* global Array:true Object:true */
-        // add includes function to array
-        if (!Array.prototype.includes) {
-            Object.defineProperty(Array.prototype, 'includes', {
-                value: function(searchElement, fromIndex) {
-                    if (this == null)
-                        throw new TypeError('"this" is null or not defined')
-                    var o = Object(this)
-                    var len = o.length >>> 0
-                    if (len === 0) return false
-                    var n = fromIndex | 0
-                    var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0)
-                    while (k < len) {
-                        if (o[k] === searchElement)
-                            return true
-                        k++
-                    }
-                    return false
-                }
-            })
+  el: '#app',
+  data: {},
+  beforeCreate () {
+    const formData = {token: cookie.getCookie('token')}
+    this.$store.dispatch('tokenVerify', formData)
+    /* global Array:true Object:true */
+    // add includes function to array
+    if (!Array.prototype.includes) {
+      Object.defineProperty(Array.prototype, 'includes', {
+        value: function (searchElement, fromIndex) {
+          if (this == null)
+            throw new TypeError('"this" is null or not defined')
+          var o = Object(this)
+          var len = o.length >>> 0
+          if (len === 0) return false
+          var n = fromIndex | 0
+          var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0)
+          while (k < len) {
+            if (o[k] === searchElement)
+              return true
+            k++
+          }
+          return false
         }
+      })
+    }
 
-    },
-    router,
-    store,
-    components: { App }
+  },
+  router,
+  store,
+  components: {App}
 })
